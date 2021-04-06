56876ac404da0b772d3e04a3a84c172031e105ef
sjiamnocna <jancikuvmail@gmail.com>
2021-04-06 21:09:53 +0200

    Adding old code and styles, adding Input with styled-components, adding yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import Input from './components/input/input';
+import { useState } from 'react';
+import FormInput from './components/form-input/form-input.comp';
 
 function App() {
+  const [name, setName] = useState('');
+  const [sname, setSname] = useState('');
   return (
     <form className="App">
-      <Input placeholder="Jméno" />
+      <h3>Registrační formulář</h3>
+      <div className="in-put">
+        <FormInput
+          name='name'
+          type='name'
+          handleChange={e => setName(e.target.value)}
+          value={name}
+          label='Příjmení'
+          required />
+        <FormInput
+          name='name'
+          type='sname'
+          handleChange={e => setSname(e.target.value)}
+          value={sname}
+          label='Příjmení'
+          required />
+      </div>
     </form>
   );
 }
