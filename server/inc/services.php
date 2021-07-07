2a2f73c1c90aa654f98e44d864df0937d1072f1c
sjiamnocna <jancikuvmail@gmail.com>
2021-07-07 10:29:12 +0200

    Add encoding to PDO

--- server/inc/services.php
+++ server/inc/services.php
@@ -4,7 +4,7 @@ require_once './inc/src/sessionManager.class.php';
 
 $SERVICES = [
     'sessionManager' => new App\session(),
-    'pdo' => new PDO('mysql:host=' . DB_HOST . ';port=3306;dbname=' . DB_NAME . ';', DB_USER, DB_PASSWORD),
+    'pdo' => new PDO('mysql:host=' . DB_HOST . ';port=3306;dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASSWORD),
     'latte' => new Latte\Engine(),
     'mailer' => new Nette\Mail\SendmailMailer
 ];
